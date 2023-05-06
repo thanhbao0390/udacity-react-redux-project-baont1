4220f80ca8123c07b895edfacb3f09c65d55a608
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2023-05-06 18:24:19 +0700

    update

--- src/features/common/ErrorPage.js
+++ src/features/common/ErrorPage.js
@@ -1,14 +1,13 @@
 import { Link, } from "react-router-dom";
+import Header from "./Header";
 
 export default function ErrorPage() {
 
   return (
     <div id="error-page">
+      <Header title='404 Page' />
       <div>
         <h1>404 Not Available</h1>
-      </div>
-      <div className="qs-btnAdd">
-        <Link to={`/home`}>List Question</Link>
       </div>
     </div>
   );
